7bd1568f5fb4209ada836388b988dc87d3ca449b
ElArahnid <eloigim@gmail.com>
2022-10-05 23:45:45 +0300

    finally edit on 4 part js

---
+++ js/animations.js
@@ -0,0 +1,92 @@
+const INCREASE_NUMBER_ANIMATION_SPEED = 50;
+const MAX_HAPPY_CLIENTS = 5000;
+const MAX_STEP_RANDOM = 100;
+
+function increaseNumberAnimationStep(i, element, endNumber) {
+  if (i <= endNumber) {
+    if (i >= endNumber - MAX_STEP_RANDOM - 1) {
+      //   element.innerText = i + "+";
+      element.innerText = MAX_HAPPY_CLIENTS + "+";
+      //   console.log(element.innerText, "<=")
+    } else {
+      element.innerText = i;
+      //   console.log(element.innerText, "=>")
+    }
+    i += Math.floor(Math.random() * (MAX_STEP_RANDOM - 1) + 1);
+    setTimeout(function () {
+      increaseNumberAnimationStep(i, element, endNumber);
+    }, INCREASE_NUMBER_ANIMATION_SPEED);
+  }
+}
+
+function initIncreaseNumberAnimation() {
+  const element = document.querySelector(".features__clients-count");
+  //   console.log(element.innerText);
+  increaseNumberAnimationStep(
+    Math.floor(Math.random() * (MAX_STEP_RANDOM - 1) + 1),
+    element,
+    MAX_HAPPY_CLIENTS
+  );
+}
+
+// initIncreaseNumberAnimation();
+
+document
+  .querySelector("#budget")
+  .addEventListener("change", function handleSelectChange(event) {
+    if (event.target.value === "other") {
+      const formContainer = document.createElement("div");
+      formContainer.classList.add("form__group");
+      formContainer.classList.add("form__other-input");
+
+      const input = document.createElement("input");
+      input.placeholder = "Введите ваш вариант";
+      input.type = "text";
+
+      formContainer.appendChild(input);
+
+      console.log(document.querySelector(".form form")); // в проектном заданиии был ("#form form")
+      document
+        .querySelector(".form form")
+        .insertBefore(formContainer, document.querySelector(".form__submit")); // ???
+    }
+    const otherInput = document.querySelector(".form__other-input");
+    if (event.target.value !== "other" && Boolean(otherInput)) {
+      document.querySelector(".form form").removeChild(otherInput);
+    }
+  });
+
+function updateScroll() {
+  // поиск позиции счетчика счастливых клиентов
+  let countElementPosition = document.querySelector(
+    ".features__clients-count"
+  ).offsetTop;
+  let windowBottomPosition = window.scrollY + window.innerHeight;
+  let animationInited = false;
+  if (windowBottomPosition >= countElementPosition && !animationInited) {
+    animationInited = true;
+    initIncreaseNumberAnimation();
+  }
+
+  if (window.scrollY > 0) {
+    document.querySelector("header").classList.add("header__scrolled");
+  } else {
+    document.querySelector("header").classList.remove("header__scrolled");
+  }
+}
+
+window.addEventListener("scroll", updateScroll);
+
+function addSmoothScroll(link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    document
+      .querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+}
+
+document.querySelectorAll('a[href^="#"]').forEach((e) => {
+  addSmoothScroll(e);
+});
+addSmoothScroll(document.querySelector('button.more-button'));
